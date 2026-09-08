e83608a3db94944fa63e04facbd0a76992db7a3f
webwynke <webwynk@gmail.com>
2026-09-08 21:18:46 +0530

    chore: stage all local changes before sync

--- assets/predictive-search.js
+++ assets/predictive-search.js
@@ -427,10 +427,6 @@ class PredictiveSearch extends SearchForm {
     let currentPage = 1;
     const totalPages = Math.ceil(totalItems / pageSize);
 
-    const prevBtn = paginationContainer.querySelector('[data-pagination-action="prev"]');
-    const nextBtn = paginationContainer.querySelector('[data-pagination-action="next"]');
-    const counter = paginationContainer.querySelector('[data-pagination-counter]');
-
     const updatePage = (page) => {
       currentPage = page;
       const startIndex = (currentPage - 1) * pageSize;
@@ -444,19 +440,26 @@ class PredictiveSearch extends SearchForm {
         }
       });
 
-      if (counter) {
-        counter.textContent = `${currentPage}/${totalPages}`;
+      const currentCounter = paginationContainer.querySelector('[data-pagination-counter]');
+      const currentPrevBtn = paginationContainer.querySelector('[data-pagination-action="prev"]');
+      const currentNextBtn = paginationContainer.querySelector('[data-pagination-action="next"]');
+
+      if (currentCounter) {
+        currentCounter.textContent = `${currentPage}/${totalPages}`;
       }
 
-      if (prevBtn) {
-        prevBtn.disabled = currentPage === 1;
+      if (currentPrevBtn) {
+        currentPrevBtn.disabled = currentPage <= 1;
       }
-      if (nextBtn) {
-        nextBtn.disabled = currentPage === totalPages;
+      if (currentNextBtn) {
+        currentNextBtn.disabled = currentPage >= totalPages;
       }
 
       this.predictiveSearchResults.scrollTop = 0;
     };
+
+    const prevBtn = paginationContainer.querySelector('[data-pagination-action="prev"]');
+    const nextBtn = paginationContainer.querySelector('[data-pagination-action="next"]');
 
     if (prevBtn && nextBtn) {
       const newPrevBtn = prevBtn.cloneNode(true);
